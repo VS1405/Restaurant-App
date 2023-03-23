e1523032ee15a2f4e401e75ef0cb93c0fc664e62
Varsha B. Mhaske <varshamhaske808@gmail.com>
2023-03-23 20:29:18 +0530

    Header, cart added

---
+++ Header.js
@@ -0,0 +1,21 @@
+import React, {Fragment} from 'react'
+
+
+import classes from './Header.module.css'
+import HeaderCartButton from './HeaderCartButton'
+
+const Header = (props) => {
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>Meals</h1>
+        <HeaderCartButton />
+      </header>
+      <div className={classes['main-image']}>
+        <img src='https://www.shutterstock.com/image-photo/arabic-cuisine-middle-eastern-traditional-260nw-1690553083.jpg' alt='A delicious food on meal'/>
+      </div>
+    </Fragment>
+  )
+}
+
+export default Header
