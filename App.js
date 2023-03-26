e089f74e70b9a19e659438cb662d6adfb0e93f58
Varsha B. Mhaske <varshamhaske808@gmail.com>
2023-03-26 20:13:27 +0530

    Cart open and close
    cart open and  close is added by using useState and used two separate handler for its working

--- App.js
+++ App.js
@@ -1,13 +1,28 @@
-import React, {Fragment} from 'react';
+import React, { Fragment, useState } from 'react';
 
 import Header from './Components/Layout/Header';
 import Meals from './Components/Meals/Meals';
+import Cart from './Components/Cart/Cart';
 
 function App() {
+
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const ShowCartHandler = ()=>{
+    setCartIsShown(true)
+  }
+  const HideCartShownHandler =()=>{
+    setCartIsShown(false);
+  }
+
+
   return (
     <Fragment>
-    <Header />
-    <Meals />
+      {cartIsShown && <Cart onClose={HideCartShownHandler}/>}
+      <main>
+        <Header onShowCart = {ShowCartHandler}/>
+        <Meals />
+      </main>
     </Fragment>
   );
 }
--- Cart.js
+++ Cart.js
@@ -9,14 +9,14 @@ const cartItem = <ul className={classes['cart-items']}>{ [{id: 'c1', name: 'sush
 const Cart = (props) => {
 
   return (
-    <Modal>
+    <Modal onClose={props.onClose}>
       {cartItem}
       <div className={classes.total}>
         <span>Total Amount</span>
         <span>35.50</span>
       </div>
       <div className={classes.actions}>
-        <button className={classes['button--alt']}>Close</button>
+        <button className={classes['button--alt']} onClick={props.onClose}>Close</button>
         <button className={classes.button}>Order</button>
       </div>
     </Modal>
